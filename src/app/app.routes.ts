0a4d428c86fed2415c968197cd5428dd3b3f4191
lilyowo <d10630818@gapps.fg.tp.edu.tw>
2026-01-29 18:58:53 +0800

    no SSR Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { LiuRen } from './liu-ren/liu-ren';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'liu-ren', component: LiuRen }
+];
